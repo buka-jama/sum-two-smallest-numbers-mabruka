cc204a16c596efb53ff2c1734bcedc9b4caf39c9
unknown <mabrukamustafam@gmail.com>
2023-08-20 20:35:40 +0300

    Answered

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Good Luck 💪🏾
+function sumTwoSmaalestNum(numb) {
+
+    let smallest= Number.MAX_VALUE;
+
+    let secondSmallest = Number.MAX_VALUE;
+
+    for (let i = 0; i < numb.length; i++) {
+        if (numb[i]<smallest) {
+
+            secondSmallest = smallest;
+
+            smallest=numb[i];
+            
+
+            
+        }else if (numb[i]<secondSmallest) {
+
+            secondSmallest = numb[i]
+            
+        }
+        
+    }
+
+    return smallest+secondSmallest;
+
+}
+console.log(sumTwoSmaalestNum([12, 5, 24, 55, 18]));
+
+console.log(sumTwoSmaalestNum([190, 300, 230, 800]));
